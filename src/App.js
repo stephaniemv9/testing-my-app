b7663e5b4cf649a26f9218472d86cb78853935af
Stephanie <stephaniemvillamar@gmail.com>
2023-06-21 13:42:25 -0400

    added rel="noreferrer" to <a> link

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
       <header className="App-header">
         <h1>Testing</h1>
       </header>
+
       <footer>
         This project was coded by Stephanie Villamar and it is
         <a
           href="https://github.com/stephaniemv9/testing-my-app"
+          rel="noreferrer"
           target="_blank"
         >
           {" "}
