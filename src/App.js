b1c2c20fabfdac3460dcb16095b45b67affcc8e8
Stephanie <stephaniemvillamar@gmail.com>
2023-06-22 13:52:13 -0400

    editted app.js
    added Cities in header. added class=search-bar. added row and col-6

--- src/App.js
+++ src/App.js
@@ -5,8 +5,46 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Testing</h1>
+          <span>New York </span>
+          <span>Quito </span>
+          <span>Rome </span>
+          <span>Madrid </span>
+          <span>Denver </span>
         </header>
+
+        <div className="search-bar">
+          <form>
+            <input
+              type="text"
+              placeholder="Enter city name"
+              autoComplete="off"
+            ></input>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="search"
+            ></input>
+            <button className="float-left btn btn-primary">
+              Current location
+            </button>
+          </form>
+        </div>
+
+        <div className="weather-summary">
+          <h2 className="search-city">New York</h2>
+          <div className="weather-date-time">Monday 12pm</div>
+          <div className="weather-detail">Sunny</div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-6">21 degrees</div>
+          <div className="col-sm-6">
+            <div className="weather-detail-three">
+              Precipitation: 82 percent
+            </div>
+            <div className="weather-detail-four">Wind: 4km/h</div>
+          </div>
+        </div>
       </div>
       <footer>
         This project was coded by Stephanie Villamar and it is{" "}
