e00e75b2838e8af80ce94f614cce92a9b93405a0
Stephanie <stephaniemvillamar@gmail.com>
2023-06-22 13:35:13 -0400

    added container to app.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Testing</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Testing</h1>
+        </header>
 
-      <footer>
-        This awesome project was coded by Stephanie Villamar and it is
-        <a
-          href="https://github.com/stephaniemv9/testing-my-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          {" "}
-          open-sourced.
-        </a>
-      </footer>
+        <footer>
+          This awesome project was coded by Stephanie Villamar and it is
+          <a
+            href="https://github.com/stephaniemv9/testing-my-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
