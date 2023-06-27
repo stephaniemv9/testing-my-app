74059d70cdbc4a4b3e51a4b1aac91222144e03f4
Stephanie <stephaniemvillamar@gmail.com>
2023-06-27 18:42:42 -0400

    updated weatherforecast.js
    and app.css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,62 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col-sm-3">
+            <div className="WeatherForecast-day">{forecast[0].dt}</div>
 
-  axios.get(apiUrl).then(handleResponse);
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+            />
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temp-max">
+                {Math.round(forecast[0].temp.max)}°C •{" "}
+              </span>
+              <span className="WeatherForecast-temp-min">
+                {Math.round(forecast[0].temp.min)}°C
+              </span>
+            </div>
+          </div>
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col-sm-3">
-          <div className="WeatherForecast-day">Thu</div>
-
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temp-max">20°C • </span>
-            <span className="WeatherForecast-temp-min">10°C</span>
+          <div className="col-sm-3">
+            Fri <br />
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+            />
+            <br />
+            21°C
+          </div>
+          <div className="col-sm-3">
+            Sat <br />
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+            />
+            <br />
+            21°C
+          </div>
+          <div className="col-sm-3">
+            Sun <br />
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="cloudy"
+            />
+            <br />
+            21°C
           </div>
         </div>
-
-        <div className="col-sm-3">
-          Fri <br />
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          <br />
-          21°C
-        </div>
-        <div className="col-sm-3">
-          Sat <br />
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          <br />
-          21°C
-        </div>
-        <div className="col-sm-3">
-          Sun <br />
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="cloudy"
-          />
-          <br />
-          21°C
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
