50a671c90c8911ae49003a2317e159f51d214354
Stephanie <stephaniemvillamar@gmail.com>
2023-06-28 15:59:36 -0400

    creates new component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -16,48 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col-sm-3">
-            <div className="WeatherForecast-day">{forecast[0].dt}</div>
-
-            <WeatherIcon code="01d" size={50} />
-
-            <div className="WeatherForecast-temp">
-              <span className="WeatherForecast-temp-max">
-                {Math.round(forecast[0].temp.max)}°C •{" "}
-              </span>
-              <span className="WeatherForecast-temp-min">
-                {Math.round(forecast[0].temp.min)}°C
-              </span>
-            </div>
-          </div>
-
-          <div className="col-sm-3">
-            Fri <br />
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-            />
-            <br />
-            21°C
-          </div>
-          <div className="col-sm-3">
-            Sat <br />
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-            />
-            <br />
-            21°C
-          </div>
-          <div className="col-sm-3">
-            Sun <br />
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-              alt="cloudy"
-            />
-            <br />
-            21°C
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
